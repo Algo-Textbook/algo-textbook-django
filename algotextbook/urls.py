2a5520d6d4c6276f43000f7495fa884d990e10c9
codingbowoo <codingbowoo@gmail.com>
2017-12-05 12:22:42 +0900

    create template files

--- algotextbook/urls.py
+++ algotextbook/urls.py
@@ -14,9 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import include, url
-from django.contrib import admin
+from django.contrib import admin\
+
+import ask.views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'', include('learn.urls'))
+    url(r'', include('learn.urls')),
+    url(r'^question/$', ask.views.question_list),
 ]
--- ask/views.py
+++ ask/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Question
 
-# Create your views here.
+
+def question_list(request):
+    questions = Question.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'ask/question_list.html', {'questions': questions})
