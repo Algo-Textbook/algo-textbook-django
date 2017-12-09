01d6cf51d091357bad018e6c73619a89178163f0
codingbowoo <codingbowoo@gmail.com>
2017-12-09 16:28:53 +0900

    fix question filter

--- ask/views.py
+++ ask/views.py
@@ -4,5 +4,5 @@ from .models import Question
 
 
 def question_list(request):
-    questions = Question.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    questions = Question.objects.filter().order_by()
     return render(request, 'ask/question_list.html', {'questions': questions})
