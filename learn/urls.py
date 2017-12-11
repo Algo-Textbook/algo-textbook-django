26385ae3b1b59015bbd023d7e52b95fad955c12a
codingbowoo <codingbowoo@gmail.com>
2017-12-11 20:16:53 +0900

    add structure of section 1

--- learn/urls.py
+++ learn/urls.py
@@ -3,4 +3,12 @@ from . import views
 
 urlpatterns = [
     url(r'^home/', views.learn_home, name='learn_home'),
+    url(r'^understand/1', views.u1_definition, name='u1'),
+    url(r'^understand/2', views.u2_computer, name='u2'),
+    url(r'^understand/3', views.u3_importance, name='u3'),
+    url(r'^understand/4', views.u4_conditions, name='u4'),
+    url(r'^understand/5', views.u5_practice, name='u5'),
+    url(r'^understand/test', views.u6_test, name='u6'),
+
 ]
+
--- learn/views.py
+++ learn/views.py
@@ -12,3 +12,28 @@ def learn_home(request):
 
 def group_activity(request):
     return render(request, 'learn/activity.html', {})
+
+
+def u1_definition(request):
+    return render(request, 'understand/u1_definition.html', {})
+
+
+def u2_computer(request):
+    return render(request, 'understand/u2_computer.html', {})
+
+
+def u3_importance(request):
+    return render(request, 'understand/u3_importance.html', {})
+
+
+def u4_conditions(request):
+    return render(request, 'understand/u4_conditions.html', {})
+
+
+def u5_practice(request):
+    return render(request, 'understand/u5_pratice.html', {})
+
+
+def u6_test(request):
+    return render(request, 'understand/u6_test.html', {})
+
