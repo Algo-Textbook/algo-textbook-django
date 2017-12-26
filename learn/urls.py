d899e364ca54b971b3d1540964de55e6b6ab1852
codingbowoo <codingbowoo@gmail.com>
2017-12-26 20:29:30 +0900

    update view & url

--- learn/urls.py
+++ learn/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     url(r'^understand/4', views.u4_conditions, name='u4'),
     url(r'^understand/5', views.u5_practice, name='u5'),
     url(r'^understand/test', views.u6_test, name='u6'),
+    url(r'^expression/1', views.e1_tools, name='e1'),
+    url(r'^expression/2', views.e2_structure, name='e2'),
+    url(r'^understand/test', views.e3_practice, name='e3'),
 
 ]
-
